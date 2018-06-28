cda5bfa9ba8ad05965c12fa25e963a532c6516f6
Denis Korinsek <denis.korinsek@kamino.si>
2018-06-28 23:37:30 +0200

    Flow view setup

--- app/src/main/java/com/mag/denis/game/ui/main/actionview/FlowView.kt
+++ app/src/main/java/com/mag/denis/game/ui/main/actionview/FlowView.kt
@@ -1,19 +1,105 @@
 package com.mag.denis.game.ui.main.actionview
 
+import android.content.ClipData
 import android.content.Context
+import android.os.Build
 import android.support.constraint.ConstraintLayout
 import android.support.v4.app.FragmentManager
 import android.util.AttributeSet
+import android.view.DragEvent
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
 import com.mag.denis.game.R
+import com.mag.denis.game.ui.main.MainActivity
+import com.mag.denis.game.ui.main.view.ActionImageView
+import com.mag.denis.game.ui.main.view.PlaceholderView
+import kotlinx.android.synthetic.main.action.view.*
 
 class FlowView(context: Context, attributes: AttributeSet) : ConstraintLayout(context, attributes) {
 
     init {
         inflate(context, R.layout.action, this)
     }
+
     fun setupViews(fragmentManager: FragmentManager) {
         super.onAttachedToWindow()
 
+        val actionUp = ActionImageView(context, R.drawable.ic_arrow_upward, MainActivity.ACTION_UP)
+        val actionRight = ActionImageView(context, R.drawable.ic_arrow_right, MainActivity.ACTION_RIGHT)
+        val actionLeft = ActionImageView(context, R.drawable.ic_arrow_left, MainActivity.ACTION_LEFT)
+        val actionDown = ActionImageView(context, R.drawable.ic_arrow_down, MainActivity.ACTION_DOWN)
+
+
+        llActions.addView(actionUp)
+        llActions.addView(actionRight)
+        llActions.addView(actionLeft)
+        llActions.addView(actionDown)
+
+        llActions.setOnDragListener { v, event ->
+            getDragListener(v, event)
+        }
+
+        actionUp.setOnTouchListener { v, event ->
+            getTouchListener(v, event)
+        }
+        actionRight.setOnTouchListener { v, event ->
+            getTouchListener(v, event)
+        }
+        actionLeft.setOnTouchListener { v, event ->
+            getTouchListener(v, event)
+        }
+        actionDown.setOnTouchListener { v, event ->
+            getTouchListener(v, event)
+        }
+
+        llActionHolder.setOnDragListener { v, event ->
+            getDragListener(v, event)
+        }
     }
 
+    private fun getDragListener(v: View, e: DragEvent): Boolean {
+        if (e.action == DragEvent.ACTION_DROP || e.action == DragEvent.ACTION_DRAG_ENDED) {
+            when (e.localState) {
+                is ActionImageView -> handleActionToPlaceholderDrag(v, e)
+            }
+        }
+        return true
+    }
+
+    private fun handleActionToPlaceholderDrag(v: View, e: DragEvent) {
+        val draggedView = e.localState as ActionImageView
+        when (e.action) {
+            DragEvent.ACTION_DROP -> {
+                val owner = draggedView.parent as ViewGroup
+                owner.removeView(draggedView) //odstrani view ce ga hocemo prestavit
+                val container = v as PlaceholderView
+                container.addView(draggedView)
+                draggedView.visibility = View.VISIBLE
+            }
+            DragEvent.ACTION_DRAG_ENDED -> {
+                draggedView.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun getTouchListener(v: View, event: MotionEvent): Boolean {
+        return when {
+            event.action == MotionEvent.ACTION_DOWN -> {
+                v.visibility = View.GONE
+                val data = ClipData.newPlainText("", "")
+                val shadowBuilder = View.DragShadowBuilder(v)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    v.startDragAndDrop(data, shadowBuilder, v, 0)
+                } else {
+                    v.startDrag(data, shadowBuilder, v, 0)
+                }
+                true
+            }
+            event.action == MotionEvent.ACTION_UP -> {
+                true
+            }
+            else -> false
+        }
+    }
 }
