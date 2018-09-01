4772d5b10b5ceb68c15f7e72e4f80d43e613477a
Denis Korinsek <denis.korinsek@kamino.si>
2018-09-01 16:23:50 +0200

    Module fix

--- app/src/main/java/com/mag/denis/game/ui/main/MainModule.kt
+++ app/src/main/java/com/mag/denis/game/ui/main/MainModule.kt
@@ -1,5 +1,6 @@
 package com.mag.denis.game.ui.main
 
+import com.mag.denis.game.manager.GameManager
 import com.mag.denis.game.manager.LevelManager
 import com.mag.denis.game.ui.ActivityScope
 import dagger.Binds
@@ -9,8 +10,9 @@ import dagger.Provides
 @Module(includes = arrayOf(MainModule.MainBindModule::class))
 class MainModule {
 
-    @Provides @ActivityScope fun providePresenter(view: MainView, levelManager: LevelManager): MainPresenter {
-        return MainPresenterImpl(view, levelManager)
+    @Provides @ActivityScope
+    fun providePresenter(view: MainView, levelManager: LevelManager, gameManager: GameManager): MainPresenter {
+        return MainPresenterImpl(view, levelManager, gameManager)
     }
 
     @Module
